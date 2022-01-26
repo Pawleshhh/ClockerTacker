2c9b616e7d19e409ddeeafd4a7c1de1f72ecaaf5
Nizio-Dev <nizio.dev@gmail.com>
2022-01-26 18:36:27 +0100

    FE todo

--- Frontend/src/components/pages/user/ProjectsPage.js
+++ Frontend/src/components/pages/user/ProjectsPage.js
@@ -1,9 +1,40 @@
+import './ProjectsPage.scss';
+
 const ProjectsPage = () =>{
 
 
     return(
-        <div>
-            Projects
+        <div className="projects-page-wrapper">
+            <div className="table-wrapper">
+
+
+                <table className="table table-dark table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Projekt</th>
+                            <th scope="col">Klient</th>
+                            <th scope="col">Czas</th>
+                            <th scope="col">Grupa</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Projekt testowy</td>
+                            <td>Bogaty klient</td>
+                            <td>4h</td>
+                            <td>:)</td>
+                        </tr>
+
+                        <tr>
+                            <td>Projekt testowy 2</td>
+                            <td>Biedak</td>
+                            <td>0h</td>
+                            <td>XD</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+             </div>
         </div>
     )
 
